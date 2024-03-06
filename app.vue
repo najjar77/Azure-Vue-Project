cedaa1f83d25725161d45b4239af392bbd3da105
Amiin Najjar <najjar.amiin@gmail.com>
2024-03-06 17:12:19 +0100

    PrimeVue UI Library Added

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
+<script setup>
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Button from 'primevue/button';
+</script>
+
 <template>
+  
   <div>
     <NuxtWelcome />
   </div>
 </template>
+
+<style scoped>
+
+</style>
